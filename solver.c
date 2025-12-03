44c0199d81bd4908e6f7d420147d0a6aa6fb5156
EvGem7 <evg200003@gmail.com>
2025-12-03 02:07:11 +0100

    threading

--- solver.c
+++ solver.c
@@ -1,10 +1,9 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+#include <pthread.h>
 
 #include "solver.h"
-
-//#define DEBUG
 
 #include "words.c"
 
@@ -17,6 +16,10 @@ bool Word_equals(Word a, Word b) {
         if (a.val[i] != b.val[i]) return false;
     }
     return true;
+}
+
+Word Word_from_str(const char* str) {
+    return *(Word*)str;
 }
 
 
@@ -41,12 +44,12 @@ typedef struct {
 
 
 typedef struct {
-    Word *arr;
+    const Word *arr;
     size_t size;
 } WordArray;
 
 typedef struct {
-    Probe *arr;
+    const Probe *arr;
     size_t size;
 } ProbeArray;
 
@@ -58,7 +61,7 @@ typedef struct {
 
 CharSet get_char_set(const Color color, const ProbeArray probes) {
     CharSet result = {0};
-    for (Probe *probe = probes.arr; probe < probes.arr + probes.size; probe++) {
+    for (const Probe *probe = probes.arr; probe < probes.arr + probes.size; probe++) {
         for (int i = 0; i < WORD_LEN; i++) {
             if (probe->result.val[i] == color) {
                 CharSet_add(&result, probe->guess.val[i]);
@@ -70,7 +73,7 @@ CharSet get_char_set(const Color color, const ProbeArray probes) {
 
 CharSetArray get_char_set_array(const Color color, const ProbeArray probes) {
     CharSetArray result = {0};
-    for (Probe *probe = probes.arr; probe < probes.arr + probes.size; probe++) {
+    for (const Probe *probe = probes.arr; probe < probes.arr + probes.size; probe++) {
         for (int i = 0; i < WORD_LEN; i++) {
             if (probe->result.val[i] == color) {
                 CharSet_add(result.sets + i, probe->guess.val[i]);
@@ -107,7 +110,7 @@ size_t filter_words(Word *dst, const WordArray words, const ProbeArray probes) {
     const CharSet green_set = get_char_set(GREEN, probes);
 
     size_t count = 0;
-    for (Word *w = words.arr; w < words.arr + words.size; w++) {
+    for (const Word *w = words.arr; w < words.arr + words.size; w++) {
         bool matches = true;
         for (int i = 0; i < WORD_LEN; i++) {
             char ch = w->val[i];
@@ -144,7 +147,7 @@ size_t count_filtered_words(const WordArray words, const ProbeArray probes, Word
     const CharSet green_set = get_char_set(GREEN, probes);
 
     size_t count = 0;
-    for (Word *w = words.arr; w < words.arr + words.size; w++) {
+    for (const Word *w = words.arr; w < words.arr + words.size; w++) {
         if (Word_equals(*w, ignore)) continue;
 
         bool matches = true;
@@ -186,38 +189,93 @@ int compare_word_amount(const void* a, const void* b) {
     return wa->amount - wb->amount;
 }
 
+
+#define THREADS_COUNT 16
+#define MAX_PA_COUNT 1500
+
+typedef struct {
+    size_t guess_from;
+    size_t guess_to;
+    const Word* possible_actuals;
+    size_t pa_count;
+    size_t pa_count_reduced;
+    WordAmount* guesses;
+} ThreadInfo;
+
+void* thread_routine(void* arg) {
+    ThreadInfo* info = arg;
+    const Word* possible_actuals = info->possible_actuals;
+    size_t pa_count = info->pa_count;
+    size_t pa_count_reduced = info->pa_count_reduced;
+    WordAmount* guesses = info->guesses;
+    for (size_t guess_i = info->guess_from; guess_i < info->guess_to; guess_i++) {
+        const Word guess = WORDS[guess_i];
+        size_t possible_count = 0;
+        for (const Word* pa = possible_actuals; pa < possible_actuals + pa_count_reduced; pa++) {
+            const WordArray arr = { .arr = possible_actuals, .size = pa_count };
+            possible_count += calc_possible_count(arr, guess, *pa);
+        }
+        guesses[guess_i] = (WordAmount) { .word = guess, .amount = possible_count };
+#ifdef DEBUG
+        printf("calculated for guess = %.5s possible_count = %d\n", guesses[guess_i].word.val, guesses[guess_i].amount);
+#endif
+    }
+    return NULL;
+}
+
 Word guess_word(void) {
+    if (get_probes_count() == 0) {
+        printf("Possible words: %zu\n", WORDS_COUNT);
+        return Word_from_str("aeros");
+    }
+
     WordAmount guesses[WORDS_COUNT] = {0};
 
     Word possible_actuals[WORDS_COUNT] = {0};
-    const WordArray words_array = { .arr = (Word*)WORDS, .size = WORDS_COUNT };
+    const WordArray words_array = { .arr = WORDS, .size = WORDS_COUNT };
     const ProbeArray probes_array = { .arr = probes, .size = probes_count };
-    const size_t filtered_count  = filter_words(possible_actuals, words_array, probes_array);
+    const size_t pa_count  = filter_words(possible_actuals, words_array, probes_array);
 
-    printf("Possible words: %zu\n", filtered_count);
-    if (filtered_count < 100) {
-        for (size_t i = 0; i < filtered_count; i++) {
+    printf("Possible words: %zu\n", pa_count);
+    if (pa_count < 100) {
+        for (size_t i = 0; i < pa_count; i++) {
             printf("%.*s ", WORD_LEN, possible_actuals[i].val);
         }
         puts("");
     }
 
-    const size_t pa_count = (filtered_count > 600) ? 600 : filtered_count;
+    const size_t pa_count_reduced = (pa_count > MAX_PA_COUNT) ? MAX_PA_COUNT : pa_count;
 
-    for (size_t guess_i = 0; guess_i < WORDS_COUNT; guess_i++) {
-        const Word guess = WORDS[guess_i];
-        size_t possible_count = 0;
-        for (Word* pa = possible_actuals; pa < possible_actuals + pa_count; pa++) {
-            const WordArray arr = { .arr = possible_actuals, .size = pa_count };
-            possible_count += calc_possible_count(arr, guess, *pa);
+    pthread_t threads[THREADS_COUNT] = {0};
+    ThreadInfo info_arr[THREADS_COUNT] = {0};
+    for (int i = 0; i < THREADS_COUNT; i++) {
+        const int last_additional = (i == THREADS_COUNT - 1) ? (WORDS_COUNT % THREADS_COUNT) : 0;
+        const size_t guess_from = WORDS_COUNT / THREADS_COUNT * i;
+        const size_t guess_to = guess_from + WORDS_COUNT / THREADS_COUNT + last_additional;
+        info_arr[i] = (ThreadInfo) {
+            .guess_from = guess_from,
+            .guess_to = guess_to,
+            .possible_actuals = possible_actuals,
+            .pa_count = pa_count,
+            .pa_count_reduced = pa_count_reduced,
+            .guesses = guesses,
+        };
+        if (pthread_create(threads + i, NULL, thread_routine, info_arr + i) != 0) {
+            fprintf(stderr, "cannot create thread\n");
+            exit(-1);
         }
-        guesses[guess_i] = (WordAmount) { .word = guess, .amount = possible_count };
+    }
+    for (int i = 0; i < THREADS_COUNT; i++) {
+        if (pthread_join(threads[i], NULL) != 0) {
+            fprintf(stderr, "cannot join thread\n");
+            exit(-1);
+        }
     }
 
     qsort(guesses, WORDS_COUNT, sizeof(guesses[0]), compare_word_amount);
 
 #ifdef DEBUG
-    printf("calculated for guess = %.5s possible_count = %d\n", guesses[0].word.val, guesses[0].amount);
+    printf("most effective guess = %.5s possible_count = %d\n", guesses[0].word.val, guesses[0].amount);
 #endif
 
     return guesses[0].word;
